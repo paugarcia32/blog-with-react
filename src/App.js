9cfd8acf31eefcf79e36710b6bdf2b06f46be36f
paugarcia32 <paugarcia32@gmail.com>
2023-07-23 01:40:20 +0200

    Building homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a href='' className='logo'>My Blog</a>
+        <nav>
+          <a href=''>Login</a>
+          <a href=''>Register</a>
+        </nav>
       </header>
-    </div>
+      <div className='post'>
+        <div className='image'>
+<img alt='' src='https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/paginas-para-crear-un-blog.jpg?width=610&height=406&name=paginas-para-crear-un-blog.jpg'></img>
+        </div>
+        <div className='texts'>
+        <h2>My  Fisrt entry blog!</h2>
+        <p className='info'>
+          <a className='author'>Pau Garcia</a>
+          <time>2023-01-06 16:45</time>
+        </p>
+        <p className='summary'>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. </p>
+        </div>
+      </div>
+
+      <div className='post'>
+        <div className='image'>
+<img alt='' src='https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/paginas-para-crear-un-blog.jpg?width=610&height=406&name=paginas-para-crear-un-blog.jpg'></img>
+        </div>
+        <div className='texts'>
+        <h2>My  Fisrt entry blog!</h2>
+        <p className='info'>
+          <a className='author'>Pau Garcia</a>
+          <time>2023-01-06 16:45</time>
+        </p>
+        <p className='summary'>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. </p>
+        </div>
+      </div>
+
+      <div className='post'>
+        <div className='image'>
+<img alt='' src='https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/paginas-para-crear-un-blog.jpg?width=610&height=406&name=paginas-para-crear-un-blog.jpg'></img>
+        </div>
+        <div className='texts'>
+        <h2>My  Fisrt entry blog!</h2>
+        <p className='info'>
+          <a className='author'>Pau Garcia</a>
+          <time>2023-01-06 16:45</time>
+        </p>
+        <p className='summary'>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
